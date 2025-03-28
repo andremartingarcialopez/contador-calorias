bd4e80f542f0cb3b8c75a022404d9d3c47cb4a00
Andy <andy_garlop4@hotmail.com>
2025-03-28 09:42:54 -0600

    Creacion del header sin boton dinamico

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,17 @@ function App() {
   
   return (
     <>
-    <h1>otra prueba</h1>
+    <header className="bg-lime-500 p-5">
+      <div className="mx-auto max-w-4xl flex justify-between">
+        <h1 className=" text-white text-3xl font-bold">
+          Contador de Calorias Test
+        </h1>
+
+        <button className="border border-white px-4 rounded-xl text-white font-bold hover:cursor-pointer hover:bg-white hover:text-lime-500">
+          Reiniciar
+        </button>
+      </div>
+    </header>
       
     </>
   )
